e6924f6e63cff9da7e8f98edbcebc1c87da90bc8
HyunJi-coding <71685092+HyunJi-coding@users.noreply.github.com>
2020-10-01 16:02:29 +0900

    215. Kth Largest Element in an Array

---
+++ 215. Kth Largest Element in an Array.c
@@ -0,0 +1,22 @@
+void bubble_sort(int arr[], int count) {
+	int temp;
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = 0; j < count - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+int findKthLargest(int* nums, int numsSize, int k) {
+	bubble_sort(nums, numsSize);
+	int output = nums[numsSize - k];
+
+	return output;
+}
