03a1a75c4739c590ca84fe9b024ca6c684f30283
HyunJi-coding <71685092+HyunJi-coding@users.noreply.github.com>
2020-10-02 15:33:16 +0900

    2020.10.02

---
+++ 419. Battleships in a Board.c
@@ -0,0 +1,12 @@
+int countBattleships(char** board, int boardSize, int* boardColSize) {
+	int i, j;
+	int cnt = 0;
+
+	for (i = 0; i < boardSize; i++) {
+		for (j = 0; j < boardColSize[0]; j++) {
+			if ((board[i][j] == 'X') && (i == 0 || board[i - 1][j] == '.') && (j == 0 || board[i][j - 1] == '.'))
+				cnt++;
+		}
+	}
+	return cnt;
+}
