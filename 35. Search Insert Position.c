2b4bcc6d0377d6d95df8687b6c335e9dac6f2304
HyunJi-coding <71685092+HyunJi-coding@users.noreply.github.com>
2020-10-16 21:38:41 +0900

    35. Search Insert Position

---
+++ 35. Search Insert Position.c
@@ -0,0 +1,10 @@
+int searchInsert(int* nums, int numsSize, int target) {
+	if (target == 0) {
+		return 0;
+	}
+	for (int i = 0; i < numsSize; i++)
+		if (nums[i] >= target)
+			return i;
+
+	return numsSize;
+}
